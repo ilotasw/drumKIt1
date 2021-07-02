498c0b1d29551abb91b2f6fd6492eb6773d41057
ilotasw <39232927+ilotasw@users.noreply.github.com>
2021-07-02 09:54:39 +0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,96 @@
+
+//Detecting  button cliks
+for(var i=0; i<7; i++){
+
+ document.querySelectorAll("button")[i].addEventListener("click", function(){
+
+var buttonInnerHTML = this.innerHTML;
+makeSound(buttonInnerHTML);
+animation(buttonInnerHTML);
+
+ });
+
+}
+//Detecting keyboard press
+document.addEventListener("keypress", function(event){
+
+ makeSound(event.key);
+ animation(event.key);
+
+  }
+);
+
+// Making sounds
+function makeSound(key){
+
+  switch (key) {
+    case "w":
+    var tom1 = new Audio('sounds/tom-1.mp3');
+
+    tom1.play();
+      break;
+
+      case "a":
+
+      var tom2 = new Audio('sounds/tom-2.mp3');
+
+      tom2.play();
+
+       break;
+
+      case "s":
+
+      var tom3 = new Audio('sounds/tom-3.mp3');
+
+      tom3.play();
+
+       break;
+
+
+
+       case "d":
+
+      var tom4 = new Audio('sounds/tom-4.mp3');
+
+      tom4.play();
+
+       break;
+
+      case "j":
+
+      var snare = new Audio('sounds/snare.mp3');
+
+      snare.play();
+
+      break;
+
+      case "k":
+
+     var crash = new Audio('sounds/crash.mp3');
+
+     crash.play();
+
+      break;
+
+      case "l":
+
+     var kickBass = new Audio('sounds/kick-bass.mp3');
+
+     kickBass.play();
+
+      break;
+
+       default: console.log(buttonInnerHTML);
+
+  }
+}
+
+//Animaiton
+function animation(currentKey){
+  document.querySelector("." + currentKey).classList.add("pressed");
+
+  setTimeout(function(){
+    document.querySelector("." + currentKey).classList.remove("pressed");
+  }, 100);
+
+}
